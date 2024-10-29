a25f3e3272cef1c3259243a9fdbcf972d3406b5c
CoroianMarius <dan.coroian03@e-uvt.ro>
2024-10-29 15:42:33 +0200

    added a plot to see the evolution in real time

--- wolfram_celular_automata.py
+++ wolfram_celular_automata.py
@@ -1,6 +1,8 @@
 import numpy as np 
+import matplotlib.pyplot as plt
 import time
 
+#mapping every combination with it's assigned value
 def rule_to_binary(rule_number):
     rule_list = np.array([], dtype=int)
     while rule_number > 0:
@@ -13,6 +15,7 @@ def rule_to_binary(rule_number):
     rule = {"111":rule_list[0], "110":rule_list[1], "101":rule_list[2], "100":rule_list[3], "011":rule_list[4], "010":rule_list[5], "001":rule_list[6], "000":rule_list[7]}
     return rule
 
+#create a new array where it modifies the state
 def state_update(current_state, rule):
     new_state = np.zeros_like(current_state)
     for i in range(1, len(current_state) - 1):
@@ -20,17 +23,27 @@ def state_update(current_state, rule):
         new_state[i] = rule[trio]
     return new_state
 
+#creating the first state and showing the plot as it evolves
 def main():
-    initial_array = np.zeros(101, dtype=int)
-    initial_array[50] = 1
-    rule = rule_to_binary(30)
-    while 1==1:
-        print(initial_array)
-        print(state_update(initial_array, rule))
-        initial_array= state_update(initial_array, rule)
-        time.sleep(0.3)
+    initial_array = np.zeros(51, dtype=int)
+    initial_array[25] = 1
+    rule = rule_to_binary(176)
+    num_steps = 50
+    
+    plt.ion()
+    fig, ax = plt.subplots()
+    ax.set_axis_off()  
+    img = ax.imshow([initial_array], cmap="binary", aspect="auto")
 
+    current_state = initial_array
+    for i in range(num_steps):
+        img.set_data(np.vstack((img.get_array(), current_state)))
+        plt.draw()
+        plt.pause(0.3) 
+        current_state = state_update(current_state, rule)
 
+    plt.ioff()  
+    plt.show()  
 
 if __name__ == "__main__":
     main()
